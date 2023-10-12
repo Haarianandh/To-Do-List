21a8436fa7977ce35eb30dfcd635235f84a224ff
Haaripriya <114047501+Haarianandh@users.noreply.github.com>
2023-10-12 08:40:36 +0530

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Header = () => {
+  return (
+    <div className="header">
+      <h2>HAARI's</h2>
+      <h1>TO-DO-LIST</h1>
+    </div>
+  );
+};
+export default Header;
